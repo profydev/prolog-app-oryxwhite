e5ee0ee1d77a683749e592da8612229dfa75b14f
oryx <mtxymtxy@gmail.com>
2023-12-05 18:21:54 -0500

    Fix status badges showing wrong status text and color

--- cypress/e2e/project-list.cy.ts
+++ cypress/e2e/project-list.cy.ts
@@ -1,4 +1,4 @@
-import capitalize from "lodash/capitalize";
+// import capitalize from "lodash/capitalize";
 import mockProjects from "../fixtures/projects.json";
 
 describe("Project List", () => {
@@ -22,6 +22,7 @@ describe("Project List", () => {
 
     it("renders the projects", () => {
       const languageNames = ["React", "Node.js", "Python"];
+      const statusStrings = ["Critical", "Warning", "Stable"];
 
       // get all project cards
       cy.get("main")
@@ -32,10 +33,28 @@ describe("Project List", () => {
           cy.wrap($el).contains(languageNames[index]);
           cy.wrap($el).contains(mockProjects[index].numIssues);
           cy.wrap($el).contains(mockProjects[index].numEvents24h);
-          cy.wrap($el).contains(capitalize(mockProjects[index].status));
+          cy.wrap($el).contains(statusStrings[index]);
           cy.wrap($el)
             .find("a")
             .should("have.attr", "href", "/dashboard/issues");
+        });
+    });
+
+    it("renders correct status badge color", () => {
+      const statusStrings = ["Critical", "Warning", "Stable"];
+      const badgeColors = [
+        "rgb(254, 243, 242)",
+        "rgb(255, 250, 235)",
+        "rgb(236, 253, 243)",
+      ];
+
+      cy.get("main")
+        .find("li")
+        .each(($el, index) => {
+          cy.wrap($el)
+            .get("div")
+            .contains(statusStrings[index])
+            .should("have.css", "background-color", badgeColors[index]);
         });
     });
   });
