213327f8f3947ebda3210eba6a2c776c8883c7f2
oryx <mtxymtxy@gmail.com>
2024-01-27 16:14:14 -0500

    Fix navigation logo size issue

--- features/layout/sidebar-navigation/sidebar-navigation.tsx
+++ features/layout/sidebar-navigation/sidebar-navigation.tsx
@@ -20,6 +20,10 @@ export function SidebarNavigation() {
   const router = useRouter();
   const { isSidebarCollapsed, toggleSidebar } = useContext(NavigationContext);
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  console.log(isSidebarCollapsed);
+  console.log(isMobileMenuOpen);
+
   return (
     <div
       className={classNames(
@@ -36,14 +40,23 @@ export function SidebarNavigation() {
         <header className={styles.header}>
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
-            src={
-              isSidebarCollapsed
-                ? "/icons/logo-small.svg"
-                : "/icons/logo-large.svg"
-            }
+            src="/icons/logo-small.svg"
             alt="logo"
-            className={styles.logo}
+            className={classNames(
+              styles.logoSmall,
+              isSidebarCollapsed && styles.isCollapsed,
+            )}
           />
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src="/icons/logo-large.svg"
+            alt="logo"
+            className={classNames(
+              styles.logoLarge,
+              isSidebarCollapsed && styles.isCollapsed,
+            )}
+          />
+
           <Button
             onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}
             className={styles.menuButton}
