868c4d4d9a8930115a9155b65c69a1bf26d2f4a8
Marina Salvucci <93627129+oryxwhite@users.noreply.github.com>
2024-01-28 14:12:20 -0500

    Add header to landing page (#13)

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,11 @@
-import { Routes } from "@config/routes";
+// import { Routes } from "@config/routes";
 import styles from "./index.module.scss";
+import { Header } from "../features/layout/header/index";
 
 const IssuesPage = () => {
   return (
     <div>
-      <header className={styles.header}>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src="/icons/logo-large.svg" alt="Prolog logo" />
-        <a href={Routes.projects}>Dashboard</a>
-      </header>
+      <Header />
       <button
         className={styles.contactButton}
         onClick={() =>
