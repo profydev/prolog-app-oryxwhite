0c5f56534ddf1be533ca12b0005a5dc52f04cb4e
oryx <mtxymtxy@gmail.com>
2024-01-28 15:59:00 -0500

    Add modal to landing page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,22 +1,29 @@
 // import { Routes } from "@config/routes";
 import styles from "./index.module.scss";
 import { Header } from "../features/layout/header/index";
+import { Modal } from "../features/layout/modal/index";
+import { useState } from "react";
 
 const IssuesPage = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
   return (
     <div>
       <Header />
-      <button
-        className={styles.contactButton}
-        onClick={() =>
-          alert(
-            "Implement this in Challenge 2 - Modal:\n\nhttps://profy.dev/rjs-challenge-modal",
-          )
-        }
-      >
+      <button className={styles.contactButton} onClick={openModal}>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src="/icons/message.svg" alt="Contact" />
       </button>
+      {isModalOpen && (
+        <Modal
+          onClose={closeModal}
+          openModal={openModal}
+          closeModal={closeModal}
+        />
+      )}
     </div>
   );
 };
